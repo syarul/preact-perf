b4c0b541a10ee81b0f5cb181b9535c5ae2956bf4
syarul <hottincup@gmail.com>
2018-01-12 11:17:00 +0800

    added keet v2.2.5

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # TodoMVC Performance Benchmark
 
-<img src="img/2018-01-10.png" width="504">
+<img src="img/2018-01-12.png" width="517">
 
-_(Results for Chrome 63 + Windows 10 on Dell OptiPlex 5040)_
+_(Results for Microsoft Edge 41 + Windows 10 on Dell OptiPlex 5040)_
+
+> Performance best in Microsoft Edge, slightly lower in Chrome (Both version of keet.js does not make of difference)
 
 # Try
 https://developit.github.io/preact-perf
--- resources/tests.js
+++ resources/tests.js
@@ -203,7 +203,42 @@ Suites.push({
     ]
 });
 
+Suites.push({
+    name: 'Keet 2.2.5',
+    url: 'todomvc/keet-v2/index.html',
+    version: '2.2.5',
+    prepare: function (runner, contentWindow, contentDocument) {
+        return runner.waitForElement('#new-todo').then(function (element) {
+            element.focus();
+            return element;
+        });
+    },
+    tests: [
+        new BenchmarkTestStep('Adding' + numberOfItemsToAdd + 'Items', function (newTodo, contentWindow, contentDocument) {
+            for (var i = 0; i < numberOfItemsToAdd; i++) {
+                var inputEvent = document.createEvent('Event');
+                inputEvent.initEvent('input', true, true);
+                newTodo.value = 'Something to do ' + i;
+                newTodo.dispatchEvent(inputEvent);
 
+                var keydownEvent = document.createEvent('Event');
+                keydownEvent.initEvent('keydown', true, true);
+                keydownEvent.keyCode = 13; // VK_ENTER
+                newTodo.dispatchEvent(keydownEvent);
+            }
+        }),
+        new BenchmarkTestStep('CompletingAllItems', function (newTodo, contentWindow, contentDocument) {
+            var checkboxes = contentDocument.querySelectorAll('.toggle');
+            for (var i = 0; i < checkboxes.length; i++)
+                checkboxes[i].click();
+        }),
+        new BenchmarkTestStep('DeletingAllItems', function (newTodo, contentWindow, contentDocument) {
+            var deleteButtons = contentDocument.querySelectorAll('.destroy');
+            for (var i = 0; i < deleteButtons.length; i++)
+                deleteButtons[i].click();
+        })
+    ]
+});
 
 Suites.push({
     name: 'Om 0.5',
